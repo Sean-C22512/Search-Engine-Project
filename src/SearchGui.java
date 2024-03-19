61ff02b872049f8e1b492ace7ecc76b71d22c77a
Sean-C22512 <124142280+Sean-C22512@users.noreply.github.com>
2024-03-19 13:14:35 +0000

    Made a Basic Gui (searchGui)

--- src/SearchGui.java
+++ src/SearchGui.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//kkk
+//kk
 
 public class SearchGui implements ActionListener {
     private JLabel resultLabel;
